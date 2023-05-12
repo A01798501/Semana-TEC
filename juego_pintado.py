4eb9aceb859ca134110d89a6e5af9d48468500dd
ferponcee <119690916+ferponcee@users.noreply.github.com>
2023-05-12 15:55:19 -0600

    comentarioscompletospaint.py

--- juego_pintado.py
+++ juego_pintado.py
@@ -1,15 +1,17 @@
 from turtle import *
 from freegames import vector
 
+'''Las funciones "line", "square", "circle", "rectangle" y "triangle" se utilizan para dibujar líneas, cuadrados, círculos, rectángulos y triángulos, respectivamente. 
+Cada función toma dos argumentos, que representan el punto de inicio y el punto final de la forma a dibujar. 
+Dentro de cada función, se utilizan las funciones de Turtle para mover la tortuga a la posición correcta y dibujar la forma.'''
+
 def line(start, end):
-    "Draw line from start to end."
     up()
     goto(start.x, start.y)
     down()
     goto(end.x, end.y)
 
 def square(start, end):
-    "Draw square from start to end."
     up()
     goto(start.x, start.y)
     down()
@@ -23,7 +25,7 @@ def square(start, end):
 
 
 def circle(start, end):
-    '''función añadida que dibuja un círculo'''
+    #función creada
     rad = math.sqrt((end.x - start.x) ** 2 + (end.y - start.y) ** 2)
     center_x = start.x + (end.x - start.x) / 2
     center_y = start.y + (end.y - start.y) / 2
@@ -42,7 +44,6 @@ def circle(start, end):
 
 
 def rectangle(start, end):
-    """Función que dibuja un rectángulo de inicio a fin."""
     up()
     goto(start.x, start.y)
     down()
@@ -57,7 +58,6 @@ def rectangle(start, end):
     end_fill()
 
 def triangle(start, end):
-    """Función que dibuja un triángulo de principio a fin."""
     up()
     goto(start.x, start.y)
     down()
@@ -68,9 +68,10 @@ def triangle(start, end):
        left(120)
        
        end fill()
-
+'''La función "tap" se utiliza para manejar la entrada del usuario. Si el usuario hace clic en la ventana gráfica
+Se guarda la posición del clic como el punto de inicio o se dibuja la forma correspondiente,
+dependiendo de si ya hay un punto de inicio almacenado en el estado actual.'''
 def tap(x, y):
-    "Store starting point or draw shape."
     start = state['start']
 
     if start is None:
@@ -81,14 +82,18 @@ def tap(x, y):
         shape(start, end)
         state['start'] = None
 
+        '''La función "store" se utiliza para almacenar valores en el estado actual.
+        El estado actual se almacena como un diccionario llamado "state", que contiene las claves "start" y "shape". 
+        La clave "start" almacena el punto de inicio actual y la clave "shape" almacena la función que se utilizará para dibujar la próxima forma.'''
 def store(key, value):
-    "Store value in state at key."
     state[key] = value
 
 state = {'start': None, 'shape': line}
 setup(420, 420, 370, 0)
 onscreenclick(tap)
 listen()
+'''El usuario puede utilizar diferentes teclas para cambiar el color de la línea, deshacer la última forma dibujada y seleccionar la forma a dibujar.
+Finalmente, el bucle principal de la biblioteca Turtle se inicia con la función "done()", lo que permite que el usuario interactúe con la ventana gráfica.'''
 onkey(undo, 'u')
 onkey(lambda: color('black'), 'K')
 onkey(lambda: color('white'), 'W')
